5211262035cbed91a68e6a5be0664e21c796d44a
Volodymyr <cherk.v8@gmail.com>
2018-03-11 21:23:35 +0000

    Remark

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -5,7 +5,7 @@ $(function() {
             event.preventDefault();
             $('html, body').stop().animate({
                 scrollTop: target.offset().top
-            }, 2000);
+            }, 700);
         }
     });
 });
